64cc09b2e05c631e7657f95796f3e2f1d947c7b3
Igor <shumd100@gmail.com>
2022-11-22 23:46:33 +0400

    add handler error

--- controllers/tableController.js
+++ controllers/tableController.js
@@ -1,13 +1,21 @@
 const { tableService } = require("../services/tableService");
 
 class TableController {
-    async getHook(req, res, next) {
-      const form=await tableService.getHook(req.body);
-        return res.json(form);
+  async getHook(req, res, next) {
+    try {
+      const form = await tableService.getHook(req.body);
+      return res.json(form);
+    } catch (e) {
+      console.error(e);
     }
-    async createStockItem(req, res, next) {
-        const form=await tableService.createStockItem(req.body);
-        return res.json(form);
+  }
+  async createStockItem(req, res, next) {
+    try {
+      const form = await tableService.createStockItem(req.body);
+      return res.json(form);
+    } catch (e) {
+      console.error(e);
     }
+  }
 }
 module.exports = { tableController: new TableController() };
--- services/tableService.js
+++ services/tableService.js
@@ -1,42 +1,53 @@
-const axios = require('axios')
-let buff=new Buffer.from('tayakes173@nubotel.com:Kolobok228!');
-let base64data = buff.toString('base64');
-let header={
-    "content-type":"application/json",
-        "Authorization":' Basic '+base64data
+const axios = require("axios");
+let buff = new Buffer.from("tayakes173@nubotel.com:Kolobok228!");
+let base64data = buff.toString("base64");
+let header = {
+  "content-type": "application/json",
+  Authorization: " Basic " + base64data,
 };
 class TableService {
-    async getHook(data) {
-
-        const response = await axios.get('https://test.bpium.ru/api/webrequest/request');
-        await axios({
-            method: 'patch',
-            headers:header,
-            url: `https://test_ssa.bpium.ru/api/v1/catalogs/${data.payload.catalogId}/records/${data.payload.recordId}`,
-            data: {
-                values: {
-                    "3":response.data.value
-                },
-            }
-        });
-        return response.data
+  async getHook(data) {
+    try {
+      const response = await axios.get(
+        "https://test.bpium.ru/api/webrequest/request"
+      );
+      await axios({
+        method: "patch",
+        headers: header,
+        url: `https://test_ssa.bpium.ru/api/v1/catalogs/${data.payload.catalogId}/records/${data.payload.recordId}`,
+        data: {
+          values: {
+            3: response.data.value,
+          },
+        },
+      });
+      return response.data;
+    } catch (e) {
+      console.error(e);
     }
-    async createStockItem(data) {
-       let result= await axios({
-            method: 'post',
-            headers:header,
-            url: 'https://test_ssa.bpium.ru/api/v1/catalogs/13/records',
-            data: {
-                values: {
-                    "3": [{
-                        "catalogId": data.payload.catalogId,
-                        "recordId": data.payload.recordId
-                    }],
-                    "4":data.payload.values["3"]
-                },
-            }
-        });
-        return result.data;
+  }
+  async createStockItem(data) {
+    try {
+      let result = await axios({
+        method: "post",
+        headers: header,
+        url: "https://test_ssa.bpium.ru/api/v1/catalogs/13/records",
+        data: {
+          values: {
+            3: [
+              {
+                catalogId: data.payload.catalogId,
+                recordId: data.payload.recordId,
+              },
+            ],
+            4: data.payload.values["3"],
+          },
+        },
+      });
+      return result.data;
+    } catch (e) {
+      console.log(e);
     }
+  }
 }
 module.exports = { tableService: new TableService() };
